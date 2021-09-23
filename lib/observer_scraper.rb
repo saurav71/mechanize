d70f14d30321fd1d8c0b2c702786529a65079c4d
saurav71 <s.aurav2@gmail.com>
2021-09-23 16:02:25 -0400

    added observer article scraping

--- lib/observer_scraper.rb
+++ lib/observer_scraper.rb
@@ -1,8 +1,27 @@
 module ObserverScraper
-  def self.mechanize_result(search_value)
-    agent = Mechanize.new
-    url = 'https://observer.com/?s=' + search_value.gsub(/\W/,"+")
-    page = agent.get(url).search(".page-title")
-    page.text
+
+  def self.find_title(search_value)
+    page = mechanize_fetch_page(search_value)
+    element =  page.search(".page-title")
+    element.text
   end
+  
+  def self.find_articles(search_value)
+    page = mechanize_fetch_page(search_value)
+    elements =  page.search("article")
+    article_hash={}
+    elements.each do |element|
+      text = element.attributes["data-title"].text
+      url = element.attributes["data-url"].text
+      article_hash[text] = url
+    end  
+    article_hash 
+  end
+
+  private
+  def self.mechanize_fetch_page(search_value)
+    agent = Mechanize.new
+    agent.get('https://observer.com/?s=' + search_value.gsub(/\W/,"+"))
+  end
+
 end
--- spec/feautres/mechanize_visits_observer_website_spec.rb
+++ spec/feautres/mechanize_visits_observer_website_spec.rb
@@ -1,9 +1,17 @@
 require "rails_helper"
 
-feature "Meachanize searcges for title" do
-  result = ObserverScraper.mechanize_result('East Village')
-  
+feature "Meachanize searches for title" do
+  result = ObserverScraper.find_title('East Village')
+
   scenario "the expected value" do
      expect(result).to include "Search"
   end
+end
+
+feature "Meachanize searches for articles" do
+    article_hash = ObserverScraper.find_articles('East Village')
+  
+    scenario "the expected value" do
+       expect(article_hash).not_to be_empty
+    end
 end
